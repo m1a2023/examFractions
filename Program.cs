e758948945846b8faed3e439da3457d6b863cb58
m1a <mag2017go@gmail.com>
2023-12-30 13:59:51 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace examFractions
+{
+    class Program
+    {
+        static void Main()
+        {
+            //example:
+            Fraction fraction1 = new Fraction(1, 2);
+            Fraction fraction2 = new Fraction(3, 5);
+            
+            Fraction fraction3 = fraction1 + fraction2;
+
+            Console.WriteLine($"{fraction1} + {fraction2} = {fraction3}");
+        }
+    }
+}
---
+++ fraction.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace examFractions
+{
+    struct Fraction
+    {
+        //fields
+        public int numerator { get; set; }
+        public int denominator { get; set; }
+
+        //constructor
+        public Fraction(int _numerator, int _denominator)
+        {
+            numerator = _numerator;
+            denominator = _denominator;
+        }
+
+        //methods
+        public static Fraction operator +(Fraction x) => x;
+        public static Fraction operator -(Fraction x) 
+            => new Fraction(-x.numerator, x.denominator);
+        public static Fraction operator +(Fraction x, Fraction y)
+        {
+            return new Fraction(x.numerator * y.denominator + x.denominator * y.numerator, x.denominator * y.denominator);
+        }
+        public static Fraction operator -(Fraction x, Fraction y)
+            => x + (-y);
+        public static Fraction operator *(Fraction x, Fraction y)
+        {
+            return new Fraction(x.numerator * y.numerator, x.denominator * y.denominator);
+        }
+        public static Fraction operator /(Fraction x, Fraction y)
+        {
+            if (y.denominator == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return new Fraction(x.numerator * y.denominator, x.denominator * y.numerator);
+        }
+
+        public override string ToString() => $"{numerator}/{denominator}";
+    }
+}
